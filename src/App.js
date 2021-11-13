469cf6d6e7ea5d86ed15fbdc2a6dd1a904cd4a65
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-13 02:16:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
+import { Cta, Brand, Navbar } from './components'
+
+const App = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default App
---
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '<div className="" />
+<whatgpt></whatgpt>.css'
+
+const WhatGPT3 = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default WhatGPT3
