72cd43d083a82de44aa83231ad7f918d16b68e4b
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-13 02:49:18 +0100

    added css variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import './Apps.css'
+import './Apps.css';
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById("root"))
