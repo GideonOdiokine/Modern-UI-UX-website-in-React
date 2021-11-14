3466632b3319985ebb9b9333ae14b3ed27f04d90
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-14 09:35:51 +0100

    started working on header component

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 const Header = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <div className="gpt3__header-content__input">
+            <input type="email" name="email" id="mail" placeholder="" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
